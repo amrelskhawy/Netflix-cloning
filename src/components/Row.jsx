c65fcf2f574a898dbd29135bd7d88be8b7833bcb
amrelskhawy <amrelskhawy@gmail.com>
2022-08-16 22:57:57 +0200

    After Creating Navbar and Main Section

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,5 +1,6 @@
 import React, { useState , useEffect } from 'react'
 import axios from 'axios'
+import { MovieCard } from './MovieCard'
 
 export const Row = ({title , fetchUrl}) => {
     const [movies , setMovies] = useState([])
@@ -8,13 +9,24 @@ export const Row = ({title , fetchUrl}) => {
         axios.get(fetchUrl).then((response) => {
             setMovies(response.data.results)
         })
-    },[])
+    },[fetchUrl])
+
     
 
     console.log(movies)
-    return (
-        <>
 
-        </>
+    return (
+        <div className='flex flex-col my-4 px-4 '>
+            <h2 className='text-white md:text-xl py-4 font-bold'>{title}</h2>
+            <div className='flex relative items-center '>
+                <div id='slider'>
+                    {
+                        movies.map(movie => {
+                            return <MovieCard key={movie.id} card={movie} />
+                        })
+                    }
+                </div>
+            </div>
+        </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,10 @@ export const Home = () => {
         <>
             <Main />
             <Row title="Up Coming" fetchUrl={requests.requestUpcoming} />
+            <Row title="Popular" fetchUrl={requests.requestPopular} />
+            <Row title="Trending" fetchUrl={requests.requestTrending} />
+            <Row title="Top Rated" fetchUrl={requests.requestTopRated} />
+            <Row title="Horror" fetchUrl={requests.requestHorror} />
         </>
     )
 }
