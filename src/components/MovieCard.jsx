aecff7fdd79e9d10bf7fe018e24663db4d1cba5a
amrelskhawy <amrelskhawy@gmail.com>
2022-08-27 20:54:14 +0200

    Scrolling the row fuctionality is added Successfully

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,23 +1,23 @@
-import React, {useState} from 'react'
-import {FaHeart , FaRegHeart} from 'react-icons/fa'
+import React, { useState } from 'react'
+import { FaHeart, FaRegHeart } from 'react-icons/fa'
 
-export const MovieCard = ({card}) => {
+export const MovieCard = ({ card }) => {
     const [checked, setChecked] = useState(false);
 
     const handleCheck = () => {
         setChecked(!checked)
     }
 
-  return (
-    <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2 hover:opacity-70'>
+    return (
+        <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2 hover:opacity-70'>
 
-        <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/original/${card.backdrop_path}`} alt={card.title} />
-        <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white '>
-            <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full transition text-center'>{card?.title}</p>
-            <span onClick={handleCheck} className=' absolute top-[12%] left-[7%]'>
-                {checked ? <FaHeart /> :  <FaRegHeart />}
-            </span>
+            <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/original/${card.backdrop_path}`} alt={card.title} />
+            <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white '>
+                <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full transition text-center'>{card?.title}</p>
+                <span onClick={handleCheck} className=' absolute top-[12%] left-[7%]'>
+                    {checked ? <FaHeart /> : <FaRegHeart />}
+                </span>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,9 @@
 import React, { useState , useEffect } from 'react'
 import axios from 'axios'
 import { MovieCard } from './MovieCard'
+import {MdChevronLeft , MdChevronRight} from 'react-icons/md'
 
-export const Row = ({title , fetchUrl}) => {
+export const Row = ({title , fetchUrl, rowId}) => {
     const [movies , setMovies] = useState([])
 
     useEffect(()=> {
@@ -11,21 +12,25 @@ export const Row = ({title , fetchUrl}) => {
         })
     },[fetchUrl])
 
-    
+    const slide = (orientation) => {
+        const slider = document.getElementById(`slider ${rowId}`)
+        slider.scrollLeft = orientation === 'right' ? slider.scrollLeft + 500 : slider.scrollLeft - 500
+    }
 
     console.log(movies)
 
     return (
         <div className='flex flex-col my-4 px-4 '>
             <h2 className='text-white md:text-xl py-4 font-bold'>{title}</h2>
-            <div className='flex relative items-center '>
-                <div id='slider'>
-                    {
-                        movies.map(movie => {
+            <div className='flex relative items-center group'>
+                    <MdChevronLeft size={30} className='arrow-style left-[-5px]' onClick={()=> slide('left')}/>
+
+                <div id={`slider ${rowId}`} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+                    {movies.map(movie => {
                             return <MovieCard key={movie.id} card={movie} />
-                        })
-                    }
+                    })}
                 </div>
+                    <MdChevronRight size={30} className='arrow-style right-[-14px]' onClick={()=> slide('right')} />
             </div>
         </div>
     )
